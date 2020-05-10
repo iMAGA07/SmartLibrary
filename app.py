d945be45df2916e05b286855511da3fb0fb41125
iMAGA07 <50173898+iMAGA07@users.noreply.github.com>
2020-05-10 12:53:59 +0500

    Add files via upload

--- app.py
+++ app.py
@@ -4,7 +4,6 @@ from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager, logout_user, current_user, UserMixin, login_user
 from datetime import datetime
 from markupsafe import escape
-# from users import User
 from forms import LoginForm, RegisterForm
 
 
@@ -14,15 +13,42 @@ app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 # db = SQLAlchemy(app)
 
 
-@app.route("/login")
+@app.route('/login', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
-    return render_template("login.html", form=form)
+    #ion = db_session.create_session()
+        #user = session.query(User).filter(User.email == form.email.data).first()
+        #if user and user.check_password(form.password.data):
+            #login_user(user, remember=form.remember_me.data)
+            #return redirect("/")
+        #return render_template('login.html',
+                               #message="Неправильный логин или пароль",
+                               #form=form)
+    return render_template('login.html', title='Авторизация', form=form)
 
-@app.route("/register")
-def register():
+@app.route('/register', methods=['GET', 'POST'])
+def reqister():
     form = RegisterForm()
-    return render_template("register.html", form=form)
+    #if form.validate_on_submit():
+        #if form.password.data != form.password_again.data:
+            #return render_template('register.html', title='Регистрация',
+                                   #form=form,
+                                   #message="Пароли не совпадают")
+        #session = db_session.create_session()
+        #if session.query(User).filter(User.email == form.email.data).first():
+            #return render_template('register.html', title='Регистрация',
+                                   #form=form,
+                                   #message="Такой пользователь уже есть")
+        #user = User(
+            #name=form.name.data,
+            #email=form.email.data,
+            #about=form.about.data
+        #)
+        #user.set_password(form.password.data)
+        #session.add(user)
+        #session.commit()
+        #return redirect('/login')
+    return render_template('register.html', title='Регистрация', form=form)
 
 
 if __name__ == "__main__":
